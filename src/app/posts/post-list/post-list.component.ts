6865c43f7b3943bb61d46e6aa3d698218f1f3ada
Samuel Muli <samuelmuli95@gmail.com>
2024-03-21 17:23:46 +0300

    too much fun, lost track

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
-  imports: [],
+  imports: [
+    MatExpansionModule
+  ],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
-
+  posts = [
+    { title: 'First Post', content: 'This is the first post\'s content' },
+    { title: 'Second Post', content: 'This is the second post\'s content' },
+    { title: 'Third Post', content: 'This is the third post\'s content' }
+  ]
 }
