7b7f6f54700818d265164011caf57c5c8723fc20
Samuel Muli <samuelmuli95@gmail.com>
2024-03-22 16:35:57 +0300

    Set up post resource on server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,6 +19,8 @@ app.post('/api/posts', (req, res, next) => {
         title: req.body.title,
         content: req.body.content
     }
+
+    console.log(post)
     res.status(201).json({
         message: 'Post added successfully!',
         post: post
