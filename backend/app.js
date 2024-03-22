7bf9de5a1ae789fba2acf0a018e651063da42907
Samuel Muli <samuelmuli95@gmail.com>
2024-03-22 15:58:20 +0300

    Set up mock data in server

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use
+app.use('/api/posts',(req, res, next) => {
+    const posts = [
+        {
+            id: 'sjdncls',
+            title: 'First Post',
+            content: 'This is the first post'
+        },
+        {
+            id: 'jdccsj',
+            title: 'Second Post',
+            content: 'This is the second post'
+        }
+    ]
+    res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: posts
+    })
+})
 
 module.exports = app
