90aa3161b40b0ab701d7be684ca4ce03d47db126
Samuel Muli <samuelmuli95@gmail.com>
2024-03-25 15:32:58 +0300

    Transform response data

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Post } from './post';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,18 @@ export class PostsService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-    .subscribe((res) => {
-      this.posts = res.posts
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+    .pipe(map((postData: any) => {
+      return postData.posts.map((post: any) => {
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        }
+      })
+    }))
+    .subscribe((posts) => {
+      this.posts = posts
       this.postsUpdated.next([...this.posts])
     })
   }
