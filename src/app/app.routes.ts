8c5aad9e94128c60e033273d090e46eb1a5942bb
Samuel Muli <samuelmuli95@gmail.com>
2024-03-25 16:57:08 +0300

    Add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+
+export const routes: Routes = [
+    {
+        path: '', 
+        component: PostListComponent 
+    },
+    {
+        path: 'create', 
+        component: PostCreateComponent
+    }
+    
+];
