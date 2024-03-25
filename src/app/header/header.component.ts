69a5f5b52d3a24e617bf0d00df44634f832e853f
Samuel Muli <samuelmuli95@gmail.com>
2024-03-25 19:24:36 +0300

    Style links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     MatToolbarModule,
-    RouterModule
-
+    RouterModule,
+    MatButtonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
