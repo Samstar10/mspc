a03db140e012f1f65dadd5a743b47a87d8cd222a
Samuel Muli <samuelmuli95@gmail.com>
2024-04-02 12:42:42 +0300

    Reorganize backend routes

---
+++ backend/routes/posts.js
@@ -0,0 +1,93 @@
+const express = require('express')
+
+const Post = require('../models/post')
+
+const router = express.Router()
+
+router.post('/', (req, res, next) => {
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    })
+
+    post.save()
+    .then(createdPost => {
+        res.status(201).json({
+            message: 'Post added successfully!',
+            postId: createdPost._id
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Creating a post failed!'
+        })
+    })
+})
+
+router.put("/:id", (req, res, next) => {
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    })
+    Post.updateOne({_id: req.params.id}, post)
+    .then(result => {
+        res.status(200).json({
+            message: 'Post updated!'
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Couldn\'t update post!',
+            error: error
+        })
+    })
+})
+
+router.get('/',(req, res, next) => {
+    Post.find()
+    .then(posts => {
+        res.status(200).json({
+            message: 'Posts fetched successfully!',
+            posts: posts
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Fetching posts failed!'
+        })
+    })
+})
+
+router.get("/:id", (req, res, next) => {
+    Post.findById(req.params.id)
+    .then(post => {
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message: 'Post not found!'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Fetching post failed!'
+        })
+    })
+})
+
+router.delete("/:id", (req, res, next) => {
+    Post.deleteOne({_id: req.params.id})
+    .then(result => {
+        console.log(result)
+        res.status(200).json({
+            message: 'Post deleted!'
+        })
+    })
+    .catch(error => {
+        res.status(500).json({
+            message: 'Deleting posts failed!'
+        })
+    })
+})
+
+module.exports = router
