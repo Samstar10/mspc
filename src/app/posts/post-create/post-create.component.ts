ac8b966699ecec3222cef7bb7df702951779bf16
Samuel Muli <samuelmuli95@gmail.com>
2024-04-03 11:11:01 +0300

    Add image controls to store the image

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -41,7 +41,8 @@ export class PostCreateComponent {
   ngOnInit() {
     this.form = new FormGroup({
       title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-      content: new FormControl(null, {validators: [Validators.required]})
+      content: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: [Validators.required]})
     })
     this.route.paramMap.subscribe((params: ParamMap) => {
       if (params.has('id')) {
@@ -66,6 +67,12 @@ export class PostCreateComponent {
         this.postId = null
       }
     })
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files![0]
+    this.form.patchValue({image: file})
+    this.form.get('image')?.updateValueAndValidity()
   }
 
   onSavePost() {
