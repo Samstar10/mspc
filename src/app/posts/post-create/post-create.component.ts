461144af899afabb4d034e7948deb1bdb237c93d
Samuel Muli <samuelmuli95@gmail.com>
2024-04-03 12:20:07 +0300

    Upload files

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -88,7 +88,7 @@ export class PostCreateComponent {
     }
     this.isLoading = true
     if(this.mode === 'create') {
-      this.service.addPost(this.form.value.title, this.form.value.content)
+      this.service.addPost(this.form.value.title, this.form.value.content, this.form.value.image)
     }else {
       this.service.updatePost(this.postId, this.form.value.title, this.form.value.content)
     }
