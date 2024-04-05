b1abdec4853f0cb724442396f2c949fc148e9cb6
Samuel Muli <samuelmuli95@gmail.com>
2024-04-05 09:50:40 +0300

    Redirect users

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
 
 import { AuthData } from './auth-data';
 
@@ -12,7 +13,7 @@ export class AuthService {
   private token: string | undefined
   private authStatusListener = new Subject<boolean>()
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getToken(){
     return this.token
@@ -41,6 +42,7 @@ export class AuthService {
       if (token) {
         this.isAuthenticated = true
         this.authStatusListener.next(true)
+        this.router.navigate(['/'])
       }
     })
   }
@@ -49,5 +51,6 @@ export class AuthService {
     this.token = null!
     this.isAuthenticated = false
     this.authStatusListener.next(false)
+    this.router.navigate(['/'])
   }
 }
